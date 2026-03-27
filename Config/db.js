0a06634686e606e9f630c304dde3e1dfff98ed44
Krish <krishkumer150@gmail.com>
2026-03-27 14:23:29 +0530

    db-changes

--- Config/db.js
+++ Config/db.js
@@ -2,16 +2,16 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Bmw2004@",
-  database: "pornhb"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
   if (err) {
     console.log("Database connection failed:", err);
-  } else {
+  } else {  
     console.log("Connected to MySQL ✅");
   }
 });
