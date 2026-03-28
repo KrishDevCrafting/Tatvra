0ba9f47b42409b51f26212409930ceaa65104b56
KrishDevCrafting <krishkumer150@gmail.com>
2026-03-28 21:52:24 +0530

    feat: secured POST /products/add route with verifyToken

--- Routes/products.js
+++ Routes/products.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { verifyToken } = require("../Middleware/authMiddleware");
 const {
   createProduct,
   getProducts,
@@ -10,9 +11,9 @@ const {
 router.get("/", getProducts);
 
 // POST /products/add -> Ading a new product
-router.post("/add", createProduct);
+router.post("/add", verifyToken, createProduct);
 
 // DELETE PRODUCTS
 
-router.delete("/delete/:id", deleteProduct);
+router.delete("/delete/:id", verifyToken, deleteProduct);
 module.exports = router;
