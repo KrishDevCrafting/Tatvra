751f95c1905a5526a23d0c9a2ed2f48a69ec85c4
Krish <krishkumer150@gmail.com>
2026-04-08 13:35:52 +0530

    updated

--- Routes/users.js
+++ Routes/users.js
@@ -4,7 +4,7 @@ const { getUsers, deleteUser } = require("../Controllers/userController");
 const { verifyToken } = require("../Middleware/authMiddleware");
 
 // Protected Routes - Inme bina Token (Bouncer ko dikhaye bina) entry allowed nai hai 🔐
-router.get("/", verifyToken, getUsers);
+router.get("/" ,verifyToken,getUsers);
 router.delete("/delete/:id", verifyToken, deleteUser);
 
 module.exports = router;
