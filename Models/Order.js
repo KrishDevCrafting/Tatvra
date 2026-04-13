62ab48cca157b88a1223dca492919ac4471220a2
KrishDevCrafting <krishkumer150@gmail.com>
2026-04-13 21:56:01 +0530

    Order-Module-added

--- Models/Order.js
+++ Models/Order.js
@@ -1,5 +1,32 @@
-const db = require("../config/db");
+const db = require("../Config/db");
 
-const Orders = {};
+const Orders = {
+  createOrder: async (user_id, total_amount, shipping_address) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        "INSERT INTO orders (user_id,total_amount,shipping_address)VALUES(?,?,?)",
+        [user_id, total_amount, shipping_address],
+        (err, result) => {
+          if (err) return reject(err);
+          resolve(result);
+        },
+      );
+    });
+  },
+
+  // GetOrderByUsers
+  getOrderByUsers: async (user_id) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        "SELECT * FROM orders WHERE user_id = ?",
+        [user_id],
+        (err, result) => {
+          if (err) return reject(err);
+          else resolve(result);
+        },
+      );
+    });
+  },
+};
 
 module.exports = Orders;
